eea10397ec89204f99588dd6dbc8a7f27724fcd7
clballes <claramballester@gmail.com>
2023-07-17 22:49:17 +0200

    math library included

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include "cub3d.h"
+
 int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
-    printf("hola\n");
+    double angle = 24; // Angle in degrees
+    double radians = angle * (M_PI / 180.0); // Convert angle from degrees to radians
+    double sine = sin(radians);
+
+    printf("The sine of %lf degrees is: %lf\n", angle, sine);
+    return 0;
 }
