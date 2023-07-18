f5efdd2008abdf8173b2f02047eb677df17ad289
clballes <claramballester@gmail.com>
2023-07-18 15:45:30 +0200

    merge master

--- obj/main.d
+++
@@ -1,2 +0,0 @@
-obj/main.o: src/main.c inc/cub3d.h libft/libft.h libft/get_next_line.h \
-  inc/../mlx/mlx.h inc/keycode_hook.h src/../mlx/mlx.h
--- obj/parsing_utils.d
+++
@@ -1,2 +0,0 @@
-obj/parsing_utils.o: src/parsing_utils.c inc/cub3d.h libft/libft.h \
-  libft/get_next_line.h inc/../mlx/mlx.h inc/keycode_hook.h
