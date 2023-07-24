2271a4eed07d710539a009e727919d37c215b58c
clballes <claramballester@gmail.com>
2023-07-24 21:03:45 +0200

    parsing walls

--- src/parsing/create_map.c
+++ src/parsing/create_map.c
@@ -1,27 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_map.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clballes <clballes@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 19:59:05 by clballes          #+#    #+#             */
+/*   Updated: 2023/07/24 19:59:06 by clballes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "cub3d.h"
 
-// int check_sp(t_map *map)
-// {
-//     int i;
-//     int j;
+void	max_x(t_map *map) //max x
+{
+	int i;
+	int j;
 
-//     i = 0;
-//     while(map->map_arr[i])
-//     {
-//         j = 0;
-//         while(map->map_arr[i][j])
-//         {
-//             if (map->map_arr[i][j] == 32)
-//                 map->map_arr[i][j] = 'A';
-//             j++;   
-//         }
-//         i++;
-//     }
-//     return (0);
-// }
+	j = 0;
+	i = 0;
+	map->max_x = 0;
+	while(map->map_arr[i])
+	{
+		while(map->map_arr[i][j])
+			j++;
+		if (map->max_x < j)
+			map->max_x = j;
+		j = 0;
+		i++;
+	}
+}
 
-int check_walls(t_map *map)
+
+int check_walls_row(t_map *map)
 {
     int i;
     int j;
@@ -32,31 +43,121 @@ int check_walls(t_map *map)
         j = 0;
         while(map->map_arr[i][j])
         {
-            while (map->map_arr[i][j] == 32)
+            while (map->map_arr[i][j]) //check linea horizontal
             {
-                if (map->map_arr[i][j]) = '1';
+				if (map->map_arr[i][j] == 32 )
+				{
+					if (i == (map->rows - 1)) //i ens trobem un espai en la ulima row
+					{
+						while(map->map_arr[i - 1][j] == 32)
+							i--;
+						if (map->map_arr[i - 1][j] == '1')
+						{
+							i = map->rows - 1;
+							j++;
+						}
+						else
+							return (1);
+					}
+					else if (i == 0) //i ens trobem un espai en la primera
+					{
+						while(map->map_arr[i + 1][j] == 32)
+							i++;
+						if (map->map_arr[i + 1][j] == '1')
+						{
+							i = 0;
+							j++;
+						}
+						else
+							return (1);
+					}
+					j++;
+				}
+				else if (map->map_arr[i][j] == '1')
+					j++;
+				else
+					return (1);
             }
-            j++;   
         }
-        i++;
-    }
+		if (i == map->rows - 1)
+			break ;
+        i = map->rows - 1; // per comprobar el final de leix horizontal
+	}
+    return (0);
+}
+
+int check_walls_col(t_map *map)
+{
+	int i;
+    int j;
+
+    j = 0;
+    i = 0;
+    while (map->map_arr[i])
+    {
+        i = 0;
+        while (map->map_arr[i][j])
+        {
+            while (map->map_arr[i][j]) //check cols quan la i == 0
+            {
+				if (map->map_arr[i][j] == 32 )
+				{
+					if (j == 0) //i ens trobem un espai en la ulima row
+					{
+						while(map->map_arr[i + 1][j] == 32)
+							i++;
+						if (map->map_arr[i + 1][j] == '1')
+						{
+							j = 0;
+							i++;
+						}
+						else
+							return (1);
+					}
+					else if (j == map->max_x - 1) //en strobem en la ultima col
+					{
+						while(map->map_arr[i][j - 1] == 32)
+							j--;
+						if (map->map_arr[i][j - 1] == '1')
+						{
+							i++;
+							j = map->max_x - 1;
+						}
+						else
+							return (1);
+					}
+					j++;
+				}
+				else if (map->map_arr[i][j] == '1')
+					j++;
+				else
+					return (1);
+            }
+        }
+		if (j == map->max_x - 1)
+			break ;
+        j = map->max_x - 1; // per comprobar el final de leix horizontal
+	}
     return (0);
 }
 
 void	ft_arraymap(t_map *map)
 {
-    int i = 0;
+    // int i = 0;
 	map->map_arr = ft_split(map->map_unid, '\n'); //free aqui
+	max_x(map); //check max x
+    // if (check_walls_row(map) != 0)
+    //     write(2, "ERROR MAP NOT SURRENDEDD BY WALLS\n", 33);
+	if (check_walls_col(map) != 0)
+        write(2, "ERROR MAP NOT SURRENDEDD BY WALLS\n", 33);
     // if (check_sp(map) != 0)
     //     write(2, "ERROR MAP NOT SURRENDEDD BY DEALT WITH SP\n", 43);
-    if (check_walls(t_map *map) != 0)
-        write(2, "ERROR MAP NOT SURRENDEDD BY WALLS\n", 33);
     
-    while(i  < map->rows)
-    {
-        printf("%s\n", map->map_arr[i]);
-        i++;
-    }
+    // while(i  < map->rows)
+    // {
+    //     printf("%s\n", map->map_arr[i]);
+    //     i++;
+    // }
 
 }
 
