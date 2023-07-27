50650406136466ded287df897182f94cc42544a3
clballes <claramballester@gmail.com>
2023-07-27 11:53:51 +0200

    parsong0

--- src/parsing/create_map.c
+++ src/parsing/create_map.c
@@ -86,59 +86,98 @@ int check_walls_row(t_map *map)
     return (0);
 }
 
-int check_walls_col(t_map *map)
+// int check_walls_col(t_map *map)
+// {
+// 	// int i;
+//     // int j;
+// 	// int	len;
+
+//     // j = 0;
+// 	// len = 0;
+//     // i = 0;
+//     // while (map->map_arr[i])
+//     // {
+//     //     i = 0;
+//     //     while (map->map_arr[i][j])
+//     //     {
+//     //         while (map->map_arr[i][j]) //check cols quan la i == 0
+//     //         {
+// 	// 			if (j == 0) //i ens trobem un espai en la ulima row
+// 	// 			{
+// 	// 				while (map->map_arr[i][j] == '0')
+// 	// 					i++;
+// 	// 				while (map->map_arr[i][j] == '1')
+// 	// 				{
+// 	// 					if (map->map_arr[i][j] == '0')
+// 	// 					{
+// 	// 						i = len; //i = columna 8
+// 	// 						while (map->map_arr[i + 1][j] == '0')
+// 	// 							i++;	//suma dos = i = 10
+// 	// 						len = i - len;
+// 	// 						// si la len es 1
+// 	// 						while (j < len) //tornem a la posicio q estavem
+// 	// 						{
+// 	// 							map->map_arr[i--][j];
+// 	// 							j++;
+// 	// 						}
+// 	// 						j = 0;
+// 	// 						if (map->map_arr[i][j + 1] == '0')
+
+// 	// 					}
+// 	// 					i++;
+// 	// 				}
+// 	// 			}
+// 	// 			else if (j == map->max_x - 1) //en strobem en la ultima col
+// 	// 			{
+// 	// 				while(map->map_arr[i][j - 1] == 32)
+// 	// 					j--;
+// 	// 				if (map->map_arr[i][j - 1] == '1')
+// 	// 				{
+// 	// 					i++;
+// 	// 					j = map->max_x - 1;
+// 	// 				}
+// 	// 				else
+// 	// 					return (1);
+// 	// 			}
+// 	// 			j++;
+// 	// 		}
+// 	// 		else if (map->map_arr[i][j] == '1')
+// 	// 			j++;
+// 	// 		else
+// 	// 			return (1);
+//     //         }
+//     //     }
+// 	// 	if (j == map->max_x - 1)
+// 	// 		break ;
+//     //     j = map->max_x - 1; // per comprobar el final de leix horizontal
+// 	// }
+//     // return (0);
+// }
+
+
+void	handle_sp(t_map *map) //put spaces to 0
 {
 	int i;
-    int j;
+	int j;
 
-    j = 0;
-    i = 0;
-    while (map->map_arr[i])
-    {
-        i = 0;
-        while (map->map_arr[i][j])
-        {
-            while (map->map_arr[i][j]) //check cols quan la i == 0
-            {
-				if (map->map_arr[i][j] == 32 )
-				{
-					if (j == 0) //i ens trobem un espai en la ulima row
-					{
-						while(map->map_arr[i + 1][j] == 32)
-							i++;
-						if (map->map_arr[i + 1][j] == '1')
-						{
-							j = 0;
-							i++;
-						}
-						else
-							return (1);
-					}
-					else if (j == map->max_x - 1) //en strobem en la ultima col
-					{
-						while(map->map_arr[i][j - 1] == 32)
-							j--;
-						if (map->map_arr[i][j - 1] == '1')
-						{
-							i++;
-							j = map->max_x - 1;
-						}
-						else
-							return (1);
-					}
-					j++;
-				}
-				else if (map->map_arr[i][j] == '1')
-					j++;
-				else
-					return (1);
-            }
-        }
-		if (j == map->max_x - 1)
-			break ;
-        j = map->max_x - 1; // per comprobar el final de leix horizontal
+	i = 0;
+	while(map->map_arr[i])
+	{
+		j = 0;
+		while(map->map_arr[i][j])
+		{
+			if (map->map_arr[i][j] == 32)
+				map->map_arr[i][j] = '0';
+			j++;
+		}
+		i++;
 	}
-    return (0);
+	i = 0;
+	while(map->map_arr[i])
+	{
+		printf("%s\n", map->map_arr[i]);
+		i++;
+	}
 }
 
 void	ft_arraymap(t_map *map)
@@ -146,10 +185,12 @@ void	ft_arraymap(t_map *map)
     // int i = 0;
 	map->map_arr = ft_split(map->map_unid, '\n'); //free aqui
 	max_x(map); //check max x
-    // if (check_walls_row(map) != 0)
+    handle_sp(map);
+	// check_walls_col(map);
+	// if (check_walls_row(map) != 0)
     //     write(2, "ERROR MAP NOT SURRENDEDD BY WALLS\n", 33);
-	if (check_walls_col(map) != 0)
-        write(2, "ERROR MAP NOT SURRENDEDD BY WALLS\n", 33);
+	// if (check_walls_col(map) != 0)
+    //     write(2, "ERROR MAP NOT SURRENDEDD BY WALLS\n", 33);
     // if (check_sp(map) != 0)
     //     write(2, "ERROR MAP NOT SURRENDEDD BY DEALT WITH SP\n", 43);
     
@@ -174,7 +215,6 @@ char	*free_var(char *src, char *dest)
 int init_map(t_map *map, int fd) //hacemos open del map y guardamos en estructura el mapa, que llamara otras funciones check errores
 {
     char *line;
-    // int len;
     int i;
 
     i = 0;
@@ -198,8 +238,6 @@ int init_map(t_map *map, int fd) //hacemos open del map y guardamos en estructur
 	}
     map->rows = i;
     ft_arraymap(map);
-
-    // map->cols = len;
     return (0);
 }
 
